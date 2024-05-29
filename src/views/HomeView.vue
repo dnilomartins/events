a12ea281d0451eb385f0d4c6742575c29942ac6c
Danilo Martins <d.smartins@icloud.com>
2024-05-29 14:26:21 -0300

    import EventCard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
-<script setup></script>
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
 
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <EventCard />
+  </div>
 </template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
